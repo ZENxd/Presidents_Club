168d5589de0c88643043fa45c644d4be353fbb84
Dave Stewart <dave@metanurb.com>
2015-09-09 16:12:18 -0400

    Angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name presidentsClubApp
+ * @description
+ * # presidentsClubApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('presidentsClubApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name presidentsClubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the presidentsClubApp
+ */
+angular.module('presidentsClubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
