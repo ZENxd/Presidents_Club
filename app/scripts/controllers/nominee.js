9953a43228263c451e6ef3f591602c027cb8c184
Dave Stewart <dave@metanurb.com>
2015-10-09 07:04:29 -0400

    CSS tweaks

--- app/scripts/controllers/nominee.js
+++ app/scripts/controllers/nominee.js
@@ -32,7 +32,7 @@ angular.module('presidentsClubApp')
                 if ($scope.userForm.$valid) {
                     //Update local model for persistance
                     modelService.updateModel($scope.nomineeModel);
-                    $location.path('/'+url);
+                    $location.path('/' + url);
                 }
             };
 
@@ -40,7 +40,7 @@ angular.module('presidentsClubApp')
             $scope.back = function(url) {
                 //Update local model for persistance
                 modelService.updateModel($scope.nomineeModel);
-                $location.path('/'+url);
+                $location.path('/' + url);
             };
 
             /*
@@ -56,7 +56,7 @@ angular.module('presidentsClubApp')
                     nomineeService.postNominee($scope.nomineeModel).then(function(result) {
                         console.log(result);
                         //Goto thank you page.
-                        $location.path('/'+url);
+                        $location.path('/' + url);
                     });
                 }
             };
@@ -73,9 +73,9 @@ angular.module('presidentsClubApp')
 
         }
     ])
-/*
-    Directive to handle 70 word max in textarea validation
-*/
+    /*
+        Directive to handle 70 word max in textarea validation
+    */
     .directive('maximumWordsValidation', function() {
         return {
             require: 'ngModel',
@@ -98,3 +98,69 @@ angular.module('presidentsClubApp')
             }
         };
     });
+/*
+    .directive('currencyMask', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModelController) {
+                // Run formatting on keyup
+                var numberWithCommas = function(value, addExtraZero) {
+                    if (addExtraZero == undefined)
+                        addExtraZero = false
+                    value = value.toString();
+                    value = value.replace(/[^0-9\.]/g, "");
+                    var parts = value.split('.');
+                    parts[0] = parts[0].replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, "$&,");
+                    if (parts[1] && parts[1].length > 2) {
+                        parts[1] = parts[1].substring(0, 2);
+                    }
+                    if (addExtraZero && parts[1] && (parts[1].length === 1)) {
+                        parts[1] = parts[1] + "0"
+                    }
+                    return parts.join(".");
+                };
+                var applyFormatting = function() {
+                    var value = element.val();
+                    var original = value;
+                    if (!value || value.length == 0) {
+                        return
+                    }
+                    value = numberWithCommas(value);
+                    if (value != original) {
+                        element.val(value);
+                        element.triggerHandler('input')
+                    }
+                };
+                element.bind('keyup', function(e) {
+                    var keycode = e.keyCode;
+                    var isTextInputKey =
+                        (keycode > 47 && keycode < 58) || // number keys
+                        keycode == 32 || keycode == 8 || // spacebar or backspace
+                        (keycode > 64 && keycode < 91) || // letter keys
+                        (keycode > 95 && keycode < 112) || // numpad keys
+                        (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+                        (keycode > 218 && keycode < 223); // [\]' (in order)
+                    if (isTextInputKey) {
+                        applyFormatting();
+                    }
+                });
+                ngModelController.$parsers.push(function(value) {
+                    if (!value || value.length == 0) {
+                        return value;
+                    }
+                    value = value.toString();
+                    value = value.replace(/[^0-9\.]/g, "");
+                    return value;
+                });
+                ngModelController.$formatters.push(function(value) {
+                    if (!value || value.length == 0) {
+                        return value;
+                    }
+                    value = numberWithCommas(value, true);
+                    return value;
+                });
+            }
+        };
+    });
+*/
